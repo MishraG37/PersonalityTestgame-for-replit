d2bca68c68263b0dd7989dffa4bf49791330a7a7
Shubham Kumar <70421983+MishraG37@users.noreply.github.com>
2022-01-18 23:27:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,271 @@
+print("WELCOME TO THE PERSONALITY TEST")
+print("HERE YOU HAVE TO ANSWER 5 QUESTIONS TO KNOW YOUR PERSONALITY ")
+print("LETS GET STARTED !")
+q = int(input("QUESTION 1  DO YOU PREFER FIRE[1] OR water[2]"))
+if q == 1 :
+  w = int(input("QUESTION 2: ARE YOU A NIGHT OWL [1] OR EARLY BIRD[2]"))
+  if w == 1 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("YOUR PERSONALITY IS RATHER INTERESTING AS YOU ARE OPEN MINDED AND LOGICAL BUT AS MUCH AS TAHT YOU MIGHT BE AN IMPATIENT PERSON AS WELL AS A LAZY PERSON ALSO YOU ARE MOST LIKELY TO GO BROKE DUE TO YOUR BAD MONEY HANDLING SKILLS") 
+        elif t == 2:
+          print("YOU aRE CREATIVE AND ARE LIKELY TO HAvE  A HIGH IQ > ALSO YOU ARE INTENSE AND HAVE A FIERY pASSION") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("YOU ARE KIND OF OPINIATED OR ENTITLED BUT ITS EITHER YOU SHOW IT OR YOU HIDE IT AND YOU HAVE A HIDDEN TALENT TO BE ARTISTIC") 
+        elif t == 2:
+          print("ARTISTIC, JOYFUL AND have A 80% CHANCES TO BE RICH WHO DOEsnT WANNA BE YOU!")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("STRANGE BUT UNIQUE , YOU MIGHT BE A GENIUS BUT THATS UP TO YOU IF YOU WANT TO UPLIFT YOUR POTENTIAL!") 
+        elif t == 2:
+          print("SOCIAL BUTTERFLY , YOU CAN BE AN INFLUENCER WITH YOUR SENsE OF HUMOR AND BEAUTY ") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("YOU ARE A MULtI TALENTED PERSON AND HAVE UNIQUE TALENTS ITS EITHER NO ONE UNDERSTANDS YOU OR EVERYONE UNDERSTANDS YOU!") 
+        elif t == 2:
+          print("CALM AND HYPER AT THE sAME TIME , YOU ARE A WITTY PERSON WITH GOod SENSE OF HUMOR")   
+  elif w == 2 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))  
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("YOU CAN BE COLD AND CRITICAL BUT CAN ALSO BE A FUNNY PERSON WITH PPL U ARE COMFORTABLE WITH!") 
+        elif t == 2:
+          print("funny loud and clever everyone wants to be you!") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("introverted and also sort of recognized as a dumb person but little do they know that you have a very high iq and eq") 
+        elif t == 2:
+          print("SUCESS ISNT FOR YOU BECUZ YOU ARE A MONEY MAGNET .YOU HAVE A ONE OF A KIND PERSONALITY AND IS HARD TO REPLACE!")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("SUCESS ISNT FOR YOU BECUZ YOU ARE A MONEY MAGNET .YOU HAVE A ONE OF A KIND PERSONALITY AND IS HARD TO REPLACE!") 
+        elif t == 2:
+          print("SUCESS ISNT FOR YOU BECUZ YOU ARE A MONEY MAGNET .YOU HAVE A ONE OF A KIND PERSONALITY AND IS HARD TO REPLACE!") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("EITHER YOU ARE ATHLETIC OR A NERD OR JUST BOTH") 
+        elif t == 2:
+          print("SORT OF BOSSY AND PATIENT AND IS SCARED TO BE ALONE") 
+elif q == 2:
+  w = int(input("QUESTION 2: ARE YOU A NIGHT OWL [1] OR EARLY BIRD[2]"))
+  if w == 1 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("WHY YOU aRE THE QUIET KID WHO Can BE THE SPAWN OF SATAN WHEN PROVOKED") 
+        elif t == 2:
+          print("WHY YOU aRE THE QUIET KID WHO Can BE THE SPAWN OF SATAN WHEN PROVOKED") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("LEADER OF THE CLAN  NO ONE CAN MATCH YOUR HEAt OF GOLD AND ATTItude") 
+        elif t == 2:
+          print("LEADER OF THE CLAN  NO ONE CAN MATCH YOUR HEAt OF GOLD AND ATTItude")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("YOU ARE EITHER SENSITIVE ON THE INSIDE AND COLD OUTSIDE OR THE OPPOSITE") 
+        elif t == 2:
+          print("HARDWORKING BUT LAZY TOO ... YOUR A LITTLE BIT OF EVERYTHING") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("g") 
+        elif t == 2:
+          print("g")
+  elif w == 2 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))  
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("EMOTINALLY INTELLIGENT AND SORT OF  A GHOSTER") 
+        elif t == 2:
+          print("GHOSTER AND SORT OF THE TEACHERS PET")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("NOT MEAN BUT ALMOST EVERYONE HATES YOU ExCEPT FOR YOUR FRIENDS CUZ THEY ARE JEALOUS") 
+        elif t == 2:
+          print("TOO GOOD AND PERFECT .......") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("ARTISTIC BUT SORT OF MESSED UP AND ITS KINDA WEIRD TO BE YOU") 
+        elif t == 2:
+          print("ARTISTIC BUT SORT OF MESSED UP AND ITS KINDA WEIRD TO BE YOU")        
+          
+print("WELCOME TO THE PERSONALITY TEST")
+print("HERE YOU HAVE TO ANSWER 5 QUESTIONS TO KNOW YOUR PERSONALITY ")
+print("LETS GET STARTED !")
+#questions
+q = int(input("QUESTION 1  DO YOU PREFER FIRE[1] OR water[2]"))
+if q == 1 :
+  w = int(input("QUESTION 2: ARE YOU A NIGHT OWL [1] OR EARLY BIRD[2]"))
+  if w == 1 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          y = int(input("WHICH DO U PREFER TO GET FIRST MONEY[1] OR LOVE[2]"))
+          if y == 1 :
+            u = int(input("PICK A COLOUR ORANGE[1] OR BLUE[2]!"))
+            if u == 1 :
+              i = int(input(""))
+#alpha type personality                  
+        elif t == 2:
+          print("YOU aRE CREATIVE AND ARE LIKELY TO HAvE  A HIGH IQ > ALSO YOU ARE INTENSE AND HAVE A FIERY pASSION") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+#beta type personality        
+        if t == 1 :
+          print("YOU ARE KIND OF OPINIATED OR ENTITLED BUT ITS EITHER YOU SHOW IT OR YOU HIDE IT AND YOU HAVE A HIDDEN TALENT TO BE ARTISTIC") 
+#beta type personality             
+        elif t == 2:
+          print("ARTISTIC, JOYFUL AND have A 80% CHANCES TO BE RICH WHO DOEsnT WANNA BE YOU!")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+#gamma type personality        
+        if t == 1 :
+          print("STRANGE BUT UNIQUE , YOU MIGHT BE A GENIUS BUT THATS UP TO YOU IF YOU WANT TO UPLIFT YOUR POTENTIAL!") 
+#gamma type personality            
+        elif t == 2:
+          print("SOCIAL BUTTERFLY , YOU CAN BE AN INFLUENCER WITH YOUR SENsE OF HUMOR AND BEAUTY ") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+#most likely omega personality        
+        if t == 1 :
+          print("YOU ARE A MULtI TALENTED PERSON AND HAVE UNIQUE TALENTS ITS EITHER NO ONE UNDERSTANDS YOU OR EVERYONE UNDERSTANDS YOU!") 
+#most likely omega personality          
+        elif t == 2:
+          print("CALM AND HYPER AT THE sAME TIME , YOU ARE A WITTY PERSON WITH GOod SENSE OF HUMOR")   
+  elif w == 2 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))  
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+#alpha personality        
+        if t == 1 :
+          print("YOU CAN BE COLD AND CRITICAL BUT CAN ALSO BE A FUNNY PERSON WITH PPL U ARE COMFORTABLE WITH!")
+#alpha personality          
+        elif t == 2:
+          print("funny loud and clever everyone wants to be you!") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+#sigma or omega personality        
+        if t == 1 :
+          print("introverted and also sort of recognized as a dumb person but little do they know that you have a very high iq and eq") 
+#sigma personality       
+        elif t == 2:
+          print("SUCESS ISNT FOR YOU BECUZ YOU ARE A MONEY MAGNET .YOU HAVE A ONE OF A KIND PERSONALITY AND IS HARD TO REPLACE!")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+#sigma personality or alpha         
+        if t == 1 :
+          print("SUCESS ISNT FOR YOU BECUZ YOU ARE A MONEY MAGNET .YOU HAVE A ONE OF A KIND PERSONALITY AND IS HARD TO REPLACE!") 
+        elif t == 2:
+          print("A good looking persona with a heart of gold  PERSONALITY AND IS HARD TO REPLACE and more likely to be a  philanthropist!") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+#gamma personality        
+        if t == 1 :
+          print("EITHER YOU ARE ATHLETIC OR A NERD OR JUST BOTH") 
+        elif t == 2:
+          print("SORT OF BOSSY AND PATIENT AND IS SCARED TO BE ALONE") 
+elif q == 2:
+  w = int(input("QUESTION 2: ARE YOU A NIGHT OWL [1] OR EARLY BIRD[2]"))
+  if w == 1 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+#sigma personality        
+        if t == 1 :
+          print("WHY YOU aRE THE QUIET KID WHO Can BE THE SPAWN OF SATAN WHEN PROVOKED") 
+        elif t == 2:
+          print("WHY YOU aRE THE QUIET KID WHO Can BE THE SPAWN OF SATAN WHEN PROVOKED") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+#alpha personality        
+        if t == 1 :
+          print("LEADER OF THE CLAN  NO ONE CAN MATCH YOUR HEAt OF GOLD AND ATTItude") 
+        elif t == 2:
+          print("LEADER OF THE CLAN  NO ONE CAN MATCH YOUR HEAt OF GOLD AND ATTItude")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+#beta personality        
+        if t == 1 :
+          print("YOU ARE EITHER SENSITIVE ON THE INSIDE AND COLD OUTSIDE OR THE OPPOSITE") 
+        elif t == 2:
+          print("HARDWORKING BUT LAZY TOO ... YOUR A LITTLE BIT OF EVERYTHING") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("g") 
+        elif t == 2:
+          print("g")
+  elif w == 2 :
+    e = int(input("QUESTION 3: HOW MANY FRIENDS DO YOU HAVE MANY(30+)[1] or less(below 30 )[2]"))  
+    if e == 1 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+#omega         
+        if t == 1 :
+          print("EMOTINALLY INTELLIGENT AND SORT OF  A GHOSTER") 
+        elif t == 2:
+          print("GHOSTER AND SORT OF THE TEACHERS PET")
+    elif e == 2 :
+      r = int(input("DO YOU PREFER LOGIC[1] OR CREATIVITY[2]"))  
+      if r == 1 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))
+        if t == 1 :
+          print("NOT MEAN BUT ALMOST EVERYONE HATES YOU ExCEPT FOR YOUR FRIENDS CUZ THEY ARE JEALOUS") 
+        elif t == 2:
+          print("TOO GOOD AND PERFECT .......") 
+      elif r == 2 :
+        t = int(input("DO YOU PREFER EARTH[1] or air[2]"))  
+        if t == 1 :
+          print("ARTISTIC BUT SORT OF MESSED UP AND ITS KINDA WEIRD TO BE YOU") 
+        elif t == 2:
+          print("ARTISTIC BUT SORT OF MESSED UP AND ITS KINDA WEIRD TO BE YOU")
